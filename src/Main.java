e35fd0aade03d66c4211cabd1a0e9beba9cfd586
Alina Mihaela <ciopecalina@gmail.com>
2024-03-27 22:14:22 +0200

    Added files

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+
+        PersonsQueue theQueue = new PersonsQueue();
+
+        System.out.println(theQueue.areMorePersons());
+
+        theQueue.addPerson("Andrew");
+        theQueue.addPerson("Diana");
+        theQueue.addPerson("Lary");
+
+        System.out.println(theQueue.whoIsNext());
+
+        theQueue.addPerson("Amy");
+        theQueue.addPerson("Anna");
+
+        theQueue.servePerson();
+
+        System.out.println(theQueue.whoIsNext());
+
+        theQueue.addPerson("Bill");
+
+        while (theQueue.areMorePersons()) {
+            System.out.println(theQueue.servePerson());
+        }
+    }
+}
